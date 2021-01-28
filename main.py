706f98c89fffcd515715eb2e70752c5c6a8337f9
Kavindu <kavinduravishka98sl@gmail.com>
2021-01-28 13:29:05 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import os
 import sys
-#from pathlib import Path as gtpath
+
 
 modpath = os.getcwd() + '/'
-
-if modpath not in sys.path:
-    sys.path.append(modpath)
-
 datadirectory = modpath+'kvasir-dataset/'
 batchdirectory = modpath+'databatch/'
